8457f6dd1abe140b4b1d83125274b3873b7eec46
LennardEc <58231666+LennardEc@users.noreply.github.com>
2025-01-04 19:36:31 +0100

    Update handles.rs

--- timely/src/dataflow/operators/generic/handles.rs
+++ timely/src/dataflow/operators/generic/handles.rs
@@ -219,7 +219,7 @@ impl<'a, T: Timestamp, D, P: Push<Bundle<T, D>>> OutputHandle<'a, T, D, P> {
     /// ```
     pub fn session<'b, C: CapabilityTrait<T>>(&'b mut self, cap: &'b C) -> Session<'b, T, D, PushCounter<T, D, P>> where 'a: 'b {
         println!("In session\n Internal buffer: {:?}\n Cap {:?}", self.internal_buffer, cap.time());
-        println!("{:?}", cap.valid_for_output(&self.internal_buffer));
+        println!("  Valid: {:?}", cap.valid_for_output(&self.internal_buffer));
         assert!(cap.valid_for_output(&self.internal_buffer), "Attempted to open output session with invalid capability");
         self.push_buffer.session(cap.time())
     }
